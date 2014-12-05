0f36a4bf2b716a8a42ed3784022a85f319fe3ab5
Eder Sosa <eder.sohe@gmail.com>
2014-12-05 01:52:50 -0600

    make better logging development, testing and production

--- app.py
+++ app.py
@@ -3,23 +3,60 @@
 
 from pymongo import MongoClient
 from flask import Flask, jsonify, request
-from logging import Formatter
+from logging import Formatter, StreamHandler
 from logging.handlers import RotatingFileHandler
 from werkzeug.exceptions import default_exceptions
 from werkzeug.exceptions import HTTPException
 
 import logging
 
-def make_json_app(import_name, **kwargs):
-    app = Flask(import_name, **kwargs)
-    app.config.from_object('config')
 
-    if not app.debug:
-        handler = RotatingFileHandler('log/foo.log', maxBytes=10000, backupCount=1)
-        handler.setLevel(logging.__dict__[app.config['LOG_LEVEL'].upper()])
-        handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        app.logger.addHandler(handler)
+class LoggingFilter(object):
 
+    def __init__(self, *args):
+        self.__args = args
+
+    def filter(self, record):
+        return record.levelno in self.__args
+
+
+class API(Flask):
+    def __init__(self, *args, **kwargs):
+        super(API, self).__init__(*args, **kwargs)
+        self.config.from_object('config')
+        self.__db = MongoClient(self.config['MONGODB_URI']).suggestic_base
+
+        if self.config['ENVIRONMENT'] == 'development':
+            handler = StreamHandler()
+            handler.setLevel(logging.DEBUG)
+            handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            self.logger.addHandler(handler)
+            self.logger.setLevel(logging.DEBUG)
+
+        elif self.config['ENVIRONMENT'] == 'testing':
+            handler = StreamHandler()
+            handler.setLevel(logging.INFO)
+            handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            self.logger.addHandler(handler)
+            self.logger.setLevel(logging.INFO)
+
+        else:
+            handler = RotatingFileHandler('log/error.log', maxBytes=10000, backupCount=1)
+            handler.setLevel(logging.ERROR)
+            handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            self.logger.addHandler(handler)
+
+            handler = RotatingFileHandler('log/access.log', maxBytes=10000, backupCount=1)
+            handler.setLevel(logging.INFO)
+            handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            handler.addFilter(LoggingFilter(logging.INFO, logging.WARNING))
+            self.logger.addHandler(handler)
+            self.logger.setLevel(logging.INFO)
+
+        for code in default_exceptions.iterkeys():
+            self.error_handler_spec[None][code] = self.make_json_error
+
+    @staticmethod
     def make_json_error(ex):
 
         error = {
@@ -27,25 +64,20 @@ def make_json_app(import_name, **kwargs):
             'message': ex.message if isinstance(ex, HTTPException) else "Internal Server Error"
         }
 
-        app.logger.exception("%s %s at %s %s", error['code'], error['message'], request.method, request.url)
-
         response = jsonify(error=error, response=None)
-        response.status_code = code
+        response.status_code = error['code']
         return response
 
-    for code in default_exceptions.iterkeys():
-        print code
-        app.error_handler_spec[None][code] = make_json_error
-
-    return app
+    def db(self, collection):
+        return self.__db[collection]
 
 
-app = make_json_app(__name__)
-db = MongoClient(app.config['MONGODB_URI']).suggestic_base
+app = API(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    app.logger.debug('Esto es una prueba')
     a = 1 / 0
     return jsonify()
     # return app.send_static_file('templates/index.html')
--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 
 DEBUG = False
 
-LOG_LEVEL = 'INFO'
+ENVIRONMENT = 'development'
 
 SERVER_NAME = '127.0.0.1:5000'
 
